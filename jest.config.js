d7fe4c16268d767edabde114a1843c6cf997dbc0
Vinicius <viniciusg.r2303@gmail.com>
2026-04-17 00:11:58 -0300

    feat: adiciona configuração do Docker e pipeline de testes CI/CD

--- jest.config.js
+++ jest.config.js
@@ -5,9 +5,18 @@ const tsJestTransformCfg = createDefaultPreset().transform;
 /** @type {import("jest").Config} **/
 module.exports = {
   testEnvironment: "node",
+  // 1. Dizemos ao ts-jest para processar tanto .ts como .js
+  transform: {
+    '^.+\\.(ts|js)$': ['ts-jest', {
+      tsconfig: 'tsconfig.json',
+    }],
+  },
+  // 2. A linha mágica: "Ignora tudo na node_modules, EXCEPTO o mariadb"
   transformIgnorePatterns: [
     "node_modules/(?!(mariadb)/)"
   ],
+  // 3. Ajuda o Jest a encontrar os ficheiros corretamente
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
   transform: {
     ...tsJestTransformCfg,
   },

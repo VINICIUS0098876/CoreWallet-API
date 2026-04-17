4c1d8bcc4da391b38e3b5d6dc9b771c1b791ae72
Vinicius <viniciusg.r2303@gmail.com>
2026-04-17 00:06:24 -0300

    feat: adiciona configuração do Docker e pipeline de testes CI/CD

--- src/prisma/index.ts
+++ src/prisma/index.ts
@@ -1,9 +1,14 @@
-import { PrismaClient } from "@prisma/client/extension";
+import { PrismaClient } from "@prisma/client"; // Mudamos para o import padrão
 import { PrismaMariaDb } from "@prisma/adapter-mariadb";
+import mariadb from "mariadb";
 
-const prisma = new PrismaClient({
-  // No Prisma 7, passamos o adaptador aqui se a URL não estiver no schema
-  adapter: new PrismaMariaDb(process.env.DATABASE_URL!),
-});
+// 1. Criamos a piscina de conexões (Obrigatorio no Prisma 7)
+const pool = mariadb.createPool(process.env.DATABASE_URL!);
+
+// 2. Criamos o adaptador usando a piscina
+const adapter = new PrismaMariaDb(pool as any);
+
+// 3. Iniciamos o Prisma com esse adaptador
+const prisma = new PrismaClient({ adapter });
 
 export default prisma;
